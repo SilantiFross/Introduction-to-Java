d6b49eaf235777b4cf112d6ec70a895e9891c821
Silanti <silantifross@live.com>
2016-09-08 23:34:28 +0300

    Add task2 in method Main

--- src/com/tasks/Main.java
+++ src/com/tasks/Main.java
@@ -1,11 +1,15 @@
 package com.tasks;
 
 import com.tasks.task1.Expression;
+import com.tasks.task2.Controller;
 
 public class Main {
 
     public static void main(String[] args) {
-	    Expression expression = new Expression(0, 0);
+        Expression expression = new Expression(0, 0);
         System.out.println("Answer: " + expression.getAnswer());
+
+        Controller controller = new Controller(3, 1);
+        System.out.println(controller.checkPointIncluded());
     }
 }
